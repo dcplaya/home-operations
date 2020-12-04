e8381e11244c2a8ac6fcbd6d6d569ad6c67e7408
Drew <dcplaya@gmail.com>
2020-12-04 07:08:08 -0800

    Try and get auto installer working

--- ubuntu-autoinstall/entrypoint.sh
+++ ubuntu-autoinstall/entrypoint.sh
@@ -9,11 +9,11 @@ sed -i 's|iso/|./|g' iso/md5sum.txt
 sed -i 's|---|autoinstall ds=nocloud\\\;s=/cdrom/nocloud/ ---|g' iso/boot/grub/grub.cfg
 sed -i 's|---|autoinstall ds=nocloud;s=/cdrom/nocloud/ ---|g' iso/isolinux/txt.cfg
 
-cp autoinstall-masters.yml iso/nocloud/user-data
+cp autoinstall-node00.yml iso/nocloud/user-data
 
 xorriso -as mkisofs -r \
     -V Ubuntu\ masters\ amd64 \
-    -o /build/ubuntu-20.04.1-live-server-amd64-autoinstall-masters.iso \
+    -o /build/ubuntu-20.04.1-live-server-amd64-autoinstall-node00.iso \
     -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
     -boot-load-size 4 -boot-info-table \
     -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
@@ -21,11 +21,47 @@ xorriso -as mkisofs -r \
     -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin  \
     iso/boot iso
 
-cp autoinstall-workers.yml iso/nocloud/user-data
+cp autoinstall-node01.yml iso/nocloud/user-data
 
 xorriso -as mkisofs -r \
     -V Ubuntu\ workers\ amd64 \
-    -o /build/ubuntu-20.04.1-live-server-amd64-autoinstall-workers.iso \
+    -o /build/ubuntu-20.04.1-live-server-amd64-autoinstall-node01.iso \
+    -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+    -boot-load-size 4 -boot-info-table \
+    -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
+    -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \
+    -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin  \
+    iso/boot iso
+
+cp autoinstall-node02.yml iso/nocloud/user-data
+
+xorriso -as mkisofs -r \
+    -V Ubuntu\ workers\ amd64 \
+    -o /build/ubuntu-20.04.1-live-server-amd64-autoinstall-node02.iso \
+    -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+    -boot-load-size 4 -boot-info-table \
+    -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
+    -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \
+    -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin  \
+    iso/boot iso
+
+cp autoinstall-node03.yml iso/nocloud/user-data
+
+xorriso -as mkisofs -r \
+    -V Ubuntu\ workers\ amd64 \
+    -o /build/ubuntu-20.04.1-live-server-amd64-autoinstall-node03.iso \
+    -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+    -boot-load-size 4 -boot-info-table \
+    -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
+    -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \
+    -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin  \
+    iso/boot iso
+
+cp autoinstall-node04.yml iso/nocloud/user-data
+
+xorriso -as mkisofs -r \
+    -V Ubuntu\ workers\ amd64 \
+    -o /build/ubuntu-20.04.1-live-server-amd64-autoinstall-node04.iso \
     -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
     -boot-load-size 4 -boot-info-table \
     -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
